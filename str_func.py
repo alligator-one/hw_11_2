13166ea07cb70492e95bc03e0a867152857cac51
astakhov.oleg@gmail.com <Astakhov>
2024-01-08 04:40:00 +0300

    add capitalize_2 function

--- str_func.py
+++ str_func.py
@@ -1,5 +1,10 @@
 def capitalize(string):
+    '''Upper String2'''
     return string.upper()
 
+def capitalize_2(string):
+    '''Capitalized String'''
+    return string.capitalize()
+
 string = 'test'
-print(capitalize(string))
+print(capitalize_2(string))
